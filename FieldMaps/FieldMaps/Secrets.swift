4934c3188e93ef847d2ceee377dcd9cea4df2eff
ptangen <ptangen@ptangen.com>
2017-02-09 10:50:52 -0500

    Add image of map.

---
+++ FieldMaps/FieldMaps/Secrets.swift
@@ -0,0 +1,13 @@
+//
+//  Secrets.swift
+//  FieldMaps
+//
+//  Created by Paul Tangen on 2/9/17.
+//  Copyright © 2017 Paul Tangen. All rights reserved.
+//
+
+import Foundation
+
+struct Secrets {
+    static let apiKey:String = "AIzaSyDd-sHNL6ur40yMs_-O_6E5rMOHdJQDAjw"
+}
